c16e1a1d8811dd5c8d1e274ca73bb82f87810db8
João Pedro Volponi <jpvrosada@hotmail.com>
2024-10-06 22:14:16 -0300

    Iniciando Projeto viaCEP

---
+++ ViaCEP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViaCEP
+//
+//  Created by João Pedro Volponi on 06/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ViaCEPTests/ViaCEPTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViaCEPTests.swift
+//  ViaCEPTests
+//
+//  Created by João Pedro Volponi on 06/10/24.
+//
+
+import Testing
+@testable import ViaCEP
+
+struct ViaCEPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
