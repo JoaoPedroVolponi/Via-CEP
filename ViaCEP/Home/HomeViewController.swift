67df0f216285a8909a1ae758f5fb0454a680f4d2
João Pedro Volponi <jpvrosada@hotmail.com>
2024-10-08 18:20:48 -0300

    HomeViewController

--- ViaCEP/Home/HomeViewController.swift
+++ ViaCEP/Home/HomeViewController.swift
@@ -8,4 +8,25 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    let viewModel: HomeViewModelProtocol
+    var homeView: HomeView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    init(viewModel: HomeViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        homeView = HomeView(viewController: self)
+        self.view = self.homeView ?? UIView()
+    }
 }
+
